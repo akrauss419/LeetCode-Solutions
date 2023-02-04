4c295af0c890eadf0679e7091aea168af23ab632
Andy Krauss <113943487+akrauss419@users.noreply.github.com>
2023-02-03 17:40:07 -0800

    Time: 110 ms (85.13%), Space: 66 MB (30.20%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const map = new Map();
+    for (i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            let j = map.get(nums[i]);
+            if (Math.abs(i - j) <= k) {
+                return true;
+            }
+        }
+        map.set(nums[i], i);
+    }
+    return false;
+};
